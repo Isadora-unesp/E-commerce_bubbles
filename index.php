9a68bb18b580d5251e5b55d18753a86c7253e0e3
Mirella Quadros <mirella.quadros@unesp.br>
2026-08-06 10:06:47 -0300

    sei la

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta naem="viewport" content="width=device-width, initial-scale=1.0">
+            <tittle>Naturale</tittle>
+        </head>
+    <body>
+        <header>
+            <!-- Logo -->
+            <!-- barra de pesquisa-->
+            <!-- Login -->
+            <!-- Carrinho -->
+        </header>
+        <nav>
+
+        </nav>
+        <main>
+            <div class="banner-principal">
+            <!-- Texto.                                              Imagem  -->
+            <!-- botão conheça mais -->
+            </div>
+            <div class="banner-categorias">
+                                                                    <!--Botão ver todas-->
+            <!-- linha horizontal de fotos (rolagem), bolinhas-->
+            </div>
+            <div class="banner-ingredientes">
+                <!-- botao saiba mais-->
+            </div>
+            <div>
+                <!-- Rolagem, quadradinho, foto, nome, preço, botão adicionar ao carrinho.-->                                    <!-- Botão ver todas-->
+                <div class="produtos">
+
+                </div>
+                <div class="produtos">
+                    
+                </div>
+                <div class="produtos">
+                    
+                </div>
+                <div class="produtos">
+                    
+                </div>
+                <div class="produtos">
+                    
+                </div>
+                <div class="produtos">
+                    
+                </div>
+            </div>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
