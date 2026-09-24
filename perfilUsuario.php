2e3f8a7e13e48ebd1e764c095099d2c2e919d265
Isabella Meirelles Cury <ra2557015>
2026-09-24 08:56:51 -0300

    Mantém alterações locais em perfilUsuario.php

--- perfilUsuario.php
+++ perfilUsuario.php
@@ -3,11 +3,11 @@
 session_start();
 
 if (!isset($_SESSION['usuario_id'])) {
-    header("Location: ../index.php");
+    header("Location: index.php");
     exit;
 }
 
-include("../util.php");
+include("util.php");
 
 $conn = conecta();
 
@@ -48,13 +48,16 @@ $telefone = $linha['telefone'];
 </head>
 
 <body>
+    
+    <a href="index.php" class="voltar-index" aria-label="Voltar para a página inicial">
+       <span>←</span> Voltar
+   </a>
 
     <main class="container">
 
         <section class="perfil">
 
             <h1>Meu Perfil</h1>
-
             <p class="mensagem">
                 Veja suas informações pessoais
             </p>
@@ -65,12 +68,10 @@ $telefone = $linha['telefone'];
                     <span class="titulo">Nome</span>
                     <p><?php echo htmlspecialchars($nome); ?></p>
                 </div>
-
                 <div class="campo">
                     <span class="titulo">Email</span>
                     <p><?php echo htmlspecialchars($email); ?></p>
                 </div>
-
                 <div class="campo">
                     <span class="titulo">Telefone</span>
                     <p><?php echo htmlspecialchars($telefone); ?></p>
@@ -80,13 +81,9 @@ $telefone = $linha['telefone'];
 
             <div class="botoes">
 
-                <a class="editar" href="editarPerfil.php">
-                    Editar Perfil
-                </a>
+                <a class="editar" href="editarPerfil.php">Editar Perfil</a>
 
-                <a class="sair" href="logout.php">
-                    Logout
-                </a>
+                <a class="sair" href="logout.php">Logout</a>
 
             </div>
 
